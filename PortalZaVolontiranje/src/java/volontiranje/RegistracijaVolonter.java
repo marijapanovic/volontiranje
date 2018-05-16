1b58b075eee2d8f38bf0818658d959fabc1de036
Marija <marijapanovic@yahoo.com>
2018-05-16 14:29:49 +0200

    Registraciona prijava za volontera

--- PortalZaVolontiranje/src/java/volontiranje/RegistracijaVolonter.java
+++ PortalZaVolontiranje/src/java/volontiranje/RegistracijaVolonter.java
@@ -4,7 +4,8 @@ package volontiranje;
 
 import javax.faces.bean.SessionScoped;
 import java.util.Date;
-import javax.annotation.PostConstruct;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -15,7 +16,17 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class RegistracijaVolonter {
     private String imeiprezime;
-    private Date date;
+    private Date datumrodjenja;
+    private boolean uslov;
+    private String pol;
+    private String drzavljanstvo;
+    private String telefon;
+    private String ulicaibroj;
+    private String mesto;
+    private String mail;
+    private String lozinka;
+    private String slika;
+    private String cv;
 
     public String getImeiprezime() {
         return imeiprezime;
@@ -25,42 +36,122 @@ public class RegistracijaVolonter {
         this.imeiprezime = imeiprezime;
     }
 
-    public Date getDate() {
-        return date;
+    public Date getDatumrodjenja() {
+        return datumrodjenja;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setDatumrodjenja(Date datumrodjenja) {
+        this.datumrodjenja = datumrodjenja;
+    }
+
+    
+
+    public boolean isUslov() {
+        return uslov;
+    }
+
+    public void setUslov(boolean uslov) {
+        this.uslov = uslov;
+    }
+    private static Map<String, String> polovi;
+    static{
+        polovi = new LinkedHashMap<String, String>();
+        polovi.put("Zensko", "Z");
+        polovi.put("Musko", "M");
+        
+    }
+    
+
+    public String getPol() {
+        return pol;
+    }
+
+    public void setPol(String pol) {
+        this.pol = pol;
     }
     
     
-   private boolean active;
-   private boolean inactive;
 
-   
-
-    public boolean isInactive() {
-        return inactive;
+    public Map<String, String> getPolovi() {
+        return polovi;
     }
 
-    public void setInactive(boolean inactive) {
-        this.inactive = inactive;
+    public void setPolovi(Map<String, String> polovi) {
+        RegistracijaVolonter.polovi = polovi;
     }
-   
-   
-   @PostConstruct
-   public void prepareMrBean() {
-      this.active = true;
-   }
 
-   public void onActiveStatusChange() {
-      if (active) inactive = false;
-   }
+    public String getDrzavljanstvo() {
+        return drzavljanstvo;
+    }
 
-   public void onInactiveStatusChange() {
-      if (inactive) active = false;
-   }
-    
+    public void setDrzavljanstvo(String drzavljanstvo) {
+        this.drzavljanstvo = drzavljanstvo;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getAdresa() {
+        return ulicaibroj;
+    }
+
+    public void setAdresa(String adresa) {
+        this.ulicaibroj = adresa;
+    }
+
+    public String getUlicaibroj() {
+        return ulicaibroj;
+    }
+
+    public void setUlicaibroj(String ulicaibroj) {
+        this.ulicaibroj = ulicaibroj;
+    }
+
+    public String getMesto() {
+        return mesto;
+    }
+
+    public void setMesto(String mesto) {
+        this.mesto = mesto;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getSlika() {
+        return slika;
+    }
+
+    public void setSlika(String slika) {
+        this.slika = slika;
+    }
+
+    public String getCv() {
+        return cv;
+    }
+
+    public void setCv(String cv) {
+        this.cv = cv;
+    }
+
     public void RegistracijaVolontera(){
         FacesContext.getCurrentInstance().addMessage(null,
                 new FacesMessage("Uspesno ste se registrovali." ));
