cecae50f3f1c98f6128c2e2468390d61a8e0f498
Srdjan <srdjan.abadzija@gmail.com>
2018-05-28 18:26:54 +0200

    napravio u bazi tabelu za admin i logon stranicu za admina ali ne radi jer mi nije najjasnije kako je to resila tasha. Pa sutra kad se vidimo da resimo to.

---
+++ PortalZaVolontiranje/src/java/beans/Admin.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author SRDJAN
+ */
+public class Admin {
+    private int admin_id;
+    private String email;
+    private String lozinka;
+    private String imePrezime;
+    private String errorEmail;
+    private String errorPassword;
+
+    public String getErrorEmail() {
+        return errorEmail;
+    }
+
+    public void setErrorEmail(String errorEmail) {
+        this.errorEmail = errorEmail;
+    }
+
+    public String getErrorPassword() {
+        return errorPassword;
+    }
+
+    public void setErrorPassword(String errorPassword) {
+        this.errorPassword = errorPassword;
+    }
+    
+    
+
+    public int getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(int admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getImePrezime() {
+        return imePrezime;
+    }
+
+    public void setImePrezime(String imePrezime) {
+        this.imePrezime = imePrezime;
+    }
+}
+    
+    
+    
+    
+    
+ 
+            
+
