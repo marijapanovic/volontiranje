3846e39210d7e3fee4928cf84f9f7a0ff4c8bd08
Marija <marijapanovic@yahoo.com>
2018-06-04 12:01:29 +0200

    isto

--- PortalZaVolontiranje/src/java/beans/Organizacija.java
+++ PortalZaVolontiranje/src/java/beans/Organizacija.java
@@ -6,5 +6,60 @@ package beans;
  * @author Korisnik
  */
 public class Organizacija {
+    private String naziv;
+    private Mesto mestoId;
+    private String email;
+    private String ulica_broj;
+    private OblastDelovanja oblastDelovanja;
+    private String lozinka;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Mesto getMestoId() {
+        return mestoId;
+    }
+
+    public void setMestoId(Mesto mestoId) {
+        this.mestoId = mestoId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUlica_broj() {
+        return ulica_broj;
+    }
+
+    public void setUlica_broj(String ulica_broj) {
+        this.ulica_broj = ulica_broj;
+    }
+
+    public OblastDelovanja getOblastDelovanja() {
+        return oblastDelovanja;
+    }
+
+    public void setOblastDelovanja(OblastDelovanja oblastDelovanja) {
+        this.oblastDelovanja = oblastDelovanja;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+    
     
 }
