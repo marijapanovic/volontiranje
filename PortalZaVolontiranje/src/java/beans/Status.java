e1a1ceb6c4c728392533e7f8de7d242ab57abc0d
vladicaRadosavljevic <vladica.radosavljevic@yahoo.com>
2018-06-12 11:07:59 +0200

    Izmena statusa u javi

--- PortalZaVolontiranje/src/java/beans/Status.java
+++ PortalZaVolontiranje/src/java/beans/Status.java
@@ -8,9 +8,9 @@ public class Status {
     public static Map<Integer, String> statusi;
     static {
         statusi = new HashMap<>();
-        statusi.put(1, "zaposlen");
-        statusi.put(2, "nezaposlen");
-        statusi.put(3, "na skolovanju");
-        statusi.put(4, "u penziji");
+        statusi.put(1, "Zaposlen");
+        statusi.put(2, "Nezaposlen");
+        statusi.put(3, "Na Školovanju");
+        statusi.put(4, "U penziji");
     }
 }
